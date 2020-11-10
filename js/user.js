1a4dfa796bb63db6f2e0f8085206e2d15a1d0b5f
nostam <matsonho@gmail.com>
2020-11-10 16:36:15 +0100

    M3-D7 ex6

--- js/user.js
+++ js/user.js
@@ -1,4 +1,48 @@
+document.getElementsByClassName("terminal-window")[0].innerHTML = "";
+document.getElementsByClassName("header-intro")[0].innerHTML = "";
+document.getElementsByClassName("project-title-container")[0].innerHTML = "";
+document.getElementsByClassName("footer")[0].innerHTML = "";
 window.onload = function () {
-  console.log(window.localStorage.getItem(storedResult));
+  let result = JSON.parse(window.localStorage.getItem("storedResult"));
   let userId = new URLSearchParams(document.location.search).get("id");
+  let user = result.filter((r) => r.id === parseInt(userId))[0];
+  //   console.log(user);
+  document.getElementsByTagName("h1")[0].innerText = user.name;
+  document
+    .querySelector(".fa-envelope")
+    .parentElement.setAttribute("href", user.email);
+  document.getElementsByClassName("terminal-window")[0].innerHTML = `
+	<div class="statement">
+	<p class="input-statement">${user.name.split(" ")[0]}.age</p>
+	<p class="output-statement">user.</p>
+	</div>
+	<div class="statement">
+	<p class="input-statement">${user.name.split(" ")[0]}.location</p>
+	<p class="output-statement">
+	${user.address.street}, ${user.address.suite}, ${user.address.city} (${
+    user.address.zipcode
+  })</p>
+	</div>
+	<div class="statement">
+	<p class="input-statement">${user.name.split(" ")[0]}.phone</p>
+	<p class="output-statement">
+		<a href="./Orhan_Örs.pdf" target="_blank">${user.phone}</a>
+	</p>
+	</div>
+	<div class="statement">
+	<p class="input-statement">${user.name.split(" ")[0]}.website</p>
+	<p class="output-statement">${user.website}</p>
+	</div>
+	<div class="statement">
+	<p class="input-statement">${user.name.split(" ")[0]}.username</p>
+	<p class="output-statement">${user.username}</p>
+	</div>`;
+
+  //   document.getElementsByClassName(
+  //     "output-statement"
+  //   )[1].innerText = `${user[0].address.street}, ${user[0].address.suite}, ${user[0].address.city} (${user[0].address.zipcode})`;
+  //   document.getElementsByClassName("input-statement")[2].innerText =
+  //     "Phone number";
+  //   document.getElementsByClassName("output-statement")[2].innerText =
+  //     user[0].phone;
 };
